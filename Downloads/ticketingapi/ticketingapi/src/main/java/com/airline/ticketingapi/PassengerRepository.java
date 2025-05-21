7fa2bfb82d19772dd4f3a058117359705ae278c5
isilkahraman <159553856+isilkahraman@users.noreply.github.com>
2025-05-21 19:49:34 +0300

    Delete Downloads/ticketingapi directory

--- Downloads/ticketingapi/ticketingapi/src/main/java/com/airline/ticketingapi/PassengerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.airline.ticketingapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
